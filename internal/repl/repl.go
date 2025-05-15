e2629fc74c78e856f6b5172492b44ec519798e08
emad <assimad8@gmail.com>
2025-05-15 21:39:26 +0100

    add a prompt like terminal to let user enter a data to interpret it

---
+++ internal/repl/repl.go
@@ -0,0 +1,31 @@
+package repl
+
+
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"github.com/assimad8/go-interpreter/internal/token"
+	"github.com/assimad8/go-interpreter/internal/lexer"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader,out io.Writer){
+	scanner := bufio.NewScanner(in)
+
+	for{
+		fmt.Print(PROMPT)
+		scanned := scanner.Scan()
+
+		if(!scanned) {return}
+
+		line := scanner.Text()
+		lex := lexer.New(line)
+
+		for tk := lex.NextToken();tk.Type != token.EOF;tk = lex.NextToken() {
+			fmt.Printf("%v+\n",tk)
+		}
+	}
+}
